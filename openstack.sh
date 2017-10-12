403cb9a20a2ad3e6ccfa660ae0f479737cc460c6
Pierre Hanselmann <Pierre.Hanselmann@nestle.com>
2017-10-12 15:53:45 +0200

    add openstack source env file

---
+++ openstack.sh
@@ -0,0 +1,11 @@
+export OS_PROJECT_DOMAIN_NAME=CHPUDO001
+export OS_USER_DOMAIN_NAME=CHPUDO001
+export OS_PROJECT_NAME=demo
+export OS_TENANT_NAME=username
+export OS_USERNAME=username
+export OS_PASSWORD=password
+export OS_AUTH_URL=http://hqchosctrl1.nespresso.com:35357/v3
+export OS_INTERFACE=internal
+export OS_IDENTITY_API_VERSION=3
+export OS_REGION_NAME=RegionOne
+
