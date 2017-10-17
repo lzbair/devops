2e2edb4735aa2c611ef41a08024be244eea911b2
Pierre Hanselmann <Pierre.Hanselmann@nestle.com>
2017-10-17 13:01:39 +0200

    Simplify manifests

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -1,16 +1,6 @@
 # Class: class vagrant_default_classes
 # 
-class vagrant_default_classes {
-
-  class { 'base' :
-    reboot_after_patching => false,
-    is_puppetmaster       => false,
-    enable_mcollective    => false,
-  }
-
-}
 
 node 'rampuplab.nespresso.com' {
-  include vagrant_default_classes
   include rampuplab
 }
